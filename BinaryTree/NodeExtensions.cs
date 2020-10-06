2223a590ea63e946e73414b68a134131278a7f6e
Onwuzulike Emeka <onwuzulikee1@gmail.com>
2020-10-06 10:28:37 +0200

    minor refactor

--- BinaryTree/NodeExtensions.cs
+++ BinaryTree/NodeExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace BinaryTree
@@ -12,19 +13,21 @@ namespace BinaryTree
             Node currentNode = node;
             Node currrentParentNode = node.Parent;
 
-            var nextNode = node?.Children?.FirstOrDefault(x => x != null);
+            var nextNode = node?.Children?.FirstOrDefault();
             
             while (nextNode == null && currrentParentNode != null)
             {
-                var currentParentNodeChildren = currrentParentNode.Children;
-                var currentNodeIndex = currentParentNodeChildren.ToList().FindIndex(x => x.GetHashCode() == currentNode.GetHashCode());
-                nextNode = currentParentNodeChildren.Skip(currentNodeIndex + 1).Take(currentParentNodeChildren.Count() - 1 - currentNodeIndex)?.FirstOrDefault();
+                var currentParentNodeChildren = currrentParentNode.Children.ToArray();
+                var currentNodeIndex = Array.FindIndex(currentParentNodeChildren,  x => x.GetHashCode() == currentNode.GetHashCode());
+                nextNode = currentNodeIndex < currentParentNodeChildren.Count() - 1 ? currentParentNodeChildren[currentNodeIndex + 1] : null;
                 currentNode = currrentParentNode;
                 currrentParentNode = currrentParentNode.Parent;
             }
 
             return nextNode;
         }
+
+        //public static Node(this IEnumerable<Node> nodes ) GetNext
 
     }
 }
--- BinaryTree/Program.cs
+++ BinaryTree/Program.cs
@@ -13,19 +13,19 @@ namespace BinaryTree
     {
         static void Main(string[] args)
         {
-            //var root = new Node(
-            //    1,
-            //    new Node(
-            //        2, new Node(4)),
-            //    new Node(
-            //        3, new Node(5, new Node(6))));
+            var root = new Node(
+                1,
+                new Node(
+                    2, new Node(4)),
+                new Node(
+                    3, new Node(5, new Node(6))));
 
-            //var n = root;
-            //while (n != null)
-            //{
-            //    Console.WriteLine(n.Data);
-            //    n = n.Next();
-            //}
+            var n = root;
+            while (n != null)
+            {
+                Console.WriteLine(n.Data);
+                n = n.Next();
+            }
         }
     }
 
